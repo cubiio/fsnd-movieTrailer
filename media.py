1792e781ddfcb29ab5cc367a57579d7d46fa82db
Sam Atkins <sam@cubi.io>
2016-11-18 19:01:42 +0000

    style: Change code to pep8 style conventions

--- media.py
+++ media.py
@@ -9,7 +9,8 @@ class Movie():
     """
 
     def __init__(self, movie_title, movie_storyline,
-                 poster_image_url, trailer_youtube_url, movie_date, movie_stars):
+                 poster_image_url, trailer_youtube_url,
+                 movie_date, movie_stars):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image_url
